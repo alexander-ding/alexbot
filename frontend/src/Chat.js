73ac974e69a0260483523680e6a5e16976c8368e
Alex Ding <alexding123@outlook.com>
2019-06-11 14:50:23 -0400

    Initial commit

---
+++ frontend/src/Chat.js
@@ -0,0 +1,11 @@
+import {Launcher} from 'react-chat-window';
+import React from 'react';
+
+export class Chat extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messageList: []
+        };
+    }
+}
