a413f7b0a207d9c377f3d6bdc95bdaa7e0808be7
Alex Ding <alexding123@outlook.com>
2019-06-11 16:26:34 -0400

    Updates

---
+++ precache-manifest.e0beab2447181a21f71b1df53241cbc8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786518088b94069f8fd38dcb6544b547",
+    "url": "/alexbot/index.html"
+  },
+  {
+    "revision": "9868364ebb8ee482b821",
+    "url": "/alexbot/static/css/2.f25a7d98.chunk.css"
+  },
+  {
+    "revision": "cf040a4b4ff2dee7bb9e",
+    "url": "/alexbot/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "9868364ebb8ee482b821",
+    "url": "/alexbot/static/js/2.20b50aad.chunk.js"
+  },
+  {
+    "revision": "cf040a4b4ff2dee7bb9e",
+    "url": "/alexbot/static/js/main.5c8a4d96.chunk.js"
+  },
+  {
+    "revision": "6ee41538d600aad68ff6",
+    "url": "/alexbot/static/js/runtime~main.06615235.js"
+  },
+  {
+    "revision": "e0d2b7486b532288a980d0cd5d535340",
+    "url": "/alexbot/static/media/chat-icon.e0d2b748.svg"
+  },
+  {
+    "revision": "7718b3e3ff59dac728c55e725a3b0523",
+    "url": "/alexbot/static/media/logo-no-bg.7718b3e3.svg"
+  },
+  {
+    "revision": "0d062b33a8b60acb2a497afe63666b16",
+    "url": "/alexbot/static/media/notification.0d062b33.mp3"
+  }
+]);
